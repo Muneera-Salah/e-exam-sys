143cb675bddcdefb2528d85dbab2ff705138dc38
Muneera-Salah <muneera.salah@gmail.com>
2020-09-24 19:31:19 +0300

    setup jaz-form-builder package

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use jazmy\FormBuilder\Traits\HasFormBuilderTraits;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasFormBuilderTraits;
 
     /**
      * The attributes that are mass assignable.
