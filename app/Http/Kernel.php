47ad27f7a224635b21a4e63b38e019fe01fc1bff
Muneera-Salah <muneera.salah@gmail.com>
2020-09-25 18:11:44 +0300

    define roles

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin.role' => \App\Http\Middleware\Admin::class,
+        'exammarker.role' => \App\Http\Middleware\ExamMarker::class,
+        'student.role' => \App\Http\Middleware\Student::class,
     ];
 }
---
+++ app/Http/Middleware/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Student
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(auth()->user()->role_id != 3){
+            abort('404');
+        }
+        return $next($request);
+    }
+}
