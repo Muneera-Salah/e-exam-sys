be50bff2b8437fffda5da0a15d25d85aeabc7d65
Muneera-Salah <muneera.salah@gmail.com>
2020-09-26 23:09:31 +0300

    update show previw form

--- resources/views/vendor/formbuilder/forms/show.blade.php
+++ resources/views/vendor/formbuilder/forms/show.blade.php
@@ -1,100 +1,110 @@
 @extends('formbuilder::layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card rounded-0">
-                <div class="card-header">
-                    <h5 class="card-title">
-                        Form Preview for '{{ $form->name }}'
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card rounded-0">
+                    <div class="card-header">
+                        <h5 class="card-title">
+                            Form Preview for '{{ $form->name }}'
 
-                        <div class="btn-toolbar float-md-right" role="toolbar">
-                            <div class="btn-group" role="group">
+                            <div class="btn-toolbar float-md-right" role="toolbar">
+                                <div class="btn-group" role="group">
 
-                                <a href="{{ route('formbuilder::forms.index') }}" class="btn btn-primary float-md-right btn-sm back-icon">
-                                    <i class="fa fa-arrow-left"></i>
-                                </a>
-                                <a href="{{ route('formbuilder::forms.submissions.index', $form) }}" class="btn btn-primary float-md-right btn-sm data-icon">
-                                    <i class="fa fa-th-list"></i> Submissions
-                                </a>
-                                @if (Auth::user()->isAdmin())
-                                <a href="{{ route('formbuilder::forms.edit', $form) }}" class="btn btn-primary float-md-right btn-sm edit-icon">
-                                    <i class="fa fa-edit"></i> Edit
-                                </a>
-                                <a href="{{ route('formbuilder::forms.create') }}" class="btn btn-primary float-md-right btn-sm">
-                                    <i class="fa fa-plus-circle"></i> New Form
-                                </a>
-                                @endif
+                                    <a href="{{ route('formbuilder::forms.index') }}"
+                                        class="btn btn-primary float-md-right btn-sm back-icon">
+                                        <i class="fa fa-arrow-left"></i>
+                                    </a>
+                                    <a href="{{ route('formbuilder::forms.submissions.index', $form) }}"
+                                        class="btn btn-primary float-md-right btn-sm data-icon">
+                                        <i class="fa fa-th-list"></i> Submissions
+                                    </a>
+                                    @if (Auth::user()->isAdmin())
+                                        <a href="{{ route('formbuilder::forms.edit', $form) }}"
+                                            class="btn btn-primary float-md-right btn-sm edit-icon">
+                                            <i class="fa fa-edit"></i> Edit
+                                        </a>
+                                        <a href="{{ route('formbuilder::forms.create') }}"
+                                            class="btn btn-primary float-md-right btn-sm">
+                                            <i class="fa fa-plus-circle"></i> New Form
+                                        </a>
+                                    @endif
+                                </div>
                             </div>
-                        </div>
-                    </h5>
-                </div>
+                        </h5>
+                    </div>
 
-                <div class="card-body">
-                    <div id="fb-render"></div>
+                    <div class="card-body">
+                        <div id="fb-render"></div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card rounded-0">
-                <div class="card-header">
-                    <h5 class="card-title">
-                        Details
+            <div class="col-md-4">
+                <div class="card rounded-0">
+                    <div class="card-header">
+                        <h5 class="card-title">
+                            Details
+                            @if (Auth::user()->isAdmin())
+                                <button class="btn btn-primary btn-sm clipboard float-right link-icon"
+                                    data-clipboard-text="{{ route('formbuilder::form.render', $form->identifier) }}"
+                                    data-message="Copied" data-original="Copy Form URL" title="Copy form URL to clipboard">
+                                    <i class="fa fa-clipboard"></i> Copy Form URL
+                                </button>
+                            @endif
+                        </h5>
+                    </div>
 
-                        <button class="btn btn-primary btn-sm clipboard float-right link-icon" data-clipboard-text="{{ route('formbuilder::form.render', $form->identifier) }}" data-message="Copied" data-original="Copy Form URL" title="Copy form URL to clipboard">
-                            <i class="fa fa-clipboard"></i> Copy Form URL
-                        </button>
-                    </h5>
+                    <ul class="list-group list-group-flush">
+                        @if (Auth::user()->isAdmin())
+                            <li class="list-group-item">
+                                <strong>Public URL: </strong>
+                                <a href="{{ route('formbuilder::form.render', $form->identifier) }}" class="float-right"
+                                    target="_blank">
+                                    {{ $form->identifier }}
+                                </a>
+                            </li>
+                        @endif
+                        <li class="list-group-item">
+                            <strong>Course: </strong> <span class="float-right">
+                                @foreach ($courses as $course)
+                                    @if ($course->id == $form->course_id)
+                                        {{ $course->title }}
+                                    @endif
+                                @endforeach
+                            </span>
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Visibility: </strong> <span class="float-right">{{ $form->visibility }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Allows Edit: </strong>
+                            <span class="float-right">{{ $form->allowsEdit() ? 'YES' : 'NO' }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Owner: </strong> <span class="float-right">{{ $form->user->name }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Current Submissions: </strong>
+                            <span class="float-right">{{ $form->submissions_count }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Last Updated On: </strong>
+                            <span class="float-right">
+                                {{ $form->updated_at->toDayDateTimeString() }}
+                            </span>
+                        </li>
+                        <li class="list-group-item">
+                            <strong>Created On: </strong>
+                            <span class="float-right">
+                                {{ $form->created_at->toDayDateTimeString() }}
+                            </span>
+                        </li>
+                    </ul>
                 </div>
-
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <strong>Public URL: </strong>
-                        <a href="{{ route('formbuilder::form.render', $form->identifier) }}" class="float-right" target="_blank">
-                            {{$form->identifier}}
-                        </a>
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Course: </strong> <span class="float-right">
-                            @foreach ($courses as $course)
-                                @if ($course->id == $form->course_id)
-                                    {{ $course->title }}
-                                @endif
-                            @endforeach
-                    </span>
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Visibility: </strong> <span class="float-right">{{ $form->visibility }}</span>
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Allows Edit: </strong>
-                        <span class="float-right">{{ $form->allowsEdit() ? 'YES' : 'NO' }}</span>
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Owner: </strong> <span class="float-right">{{ $form->user->name }}</span>
-                    </li>
-                     <li class="list-group-item">
-                        <strong>Current Submissions: </strong>
-                        <span class="float-right">{{ $form->submissions_count }}</span>
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Last Updated On: </strong>
-                        <span class="float-right">
-                            {{ $form->updated_at->toDayDateTimeString() }}
-                        </span>
-                    </li>
-                    <li class="list-group-item">
-                        <strong>Created On: </strong>
-                        <span class="float-right">
-                            {{ $form->created_at->toDayDateTimeString() }}
-                        </span>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @push(config('formbuilder.layout_js_stack', 'scripts'))
